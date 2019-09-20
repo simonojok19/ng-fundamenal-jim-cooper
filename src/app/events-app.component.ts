90265507daa26c776a31a7bd2d0661e4b64596a5
Simon Peter Ojok <simonojok19@gmail.com>
2019-09-21 02:29:28 +0300

    started the project

---
+++ src/app/events-app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  // tslint:disable-next-line: component-selector
+  selector: 'events-app',
+  template: '<h1>Hello world</h1>',
+})
+export class EventsAppComponent {
+  title = 'ng-fundamental';
+}
