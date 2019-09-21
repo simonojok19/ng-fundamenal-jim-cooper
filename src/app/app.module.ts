6b1e0e6bbf3e39177042b6acbb114f6d94387927
Simon Peter Ojok <simonojok19@gmail.com>
2019-09-21 23:15:23 +0300

    module done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {
+  EventsListComponent,
+  EventThumbnailComponent,
+  EventService,
+  EventDetailsComponent,
+  CreateEventComponent,
+  EventRouteActivator,
+  EventListResolver
+} from './events/index';
 import { EventsAppComponent } from './events-app.component';
-import { EventsListComponent } from './events/events-list.component';
-import {EventThumbnailComponent} from './events/event-thumbnail.componet';
 import {NavBarComponent} from './nav/navbar.component';
-import { EventService } from './events/shared/events.service';
 import { ToastrService } from './common/toastr.service';
-import { EventDetailsComponent } from './events/event-details/event-details.component';
 import {RouterModule} from '@angular/router';
 import {appRoutes} from './routes';
-import {CreateEventComponent} from './events/create-event.component';
 import {Error404Component} from './errors/404.component';
-import {EventRouteActivator} from './events/event-details/event-route-activator.service';
-import { EventListResolver } from './events/events-list-resolver.service';
 
 @NgModule({
   imports: [
--- src/app/events/index.ts
+++ src/app/events/index.ts
@@ -2,3 +2,5 @@ export * from './create-event.component';
 export * from './event-thumbnail.componet';
 export * from './events-list-resolver.service';
 export * from './events-list.component';
+export * from './shared/index';
+export * from './event-details/index';
