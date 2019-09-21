1a4bbe937b3df01b090f965279a43f0d6837e7d6
Simon Peter Ojok <simonojok19@gmail.com>
2019-09-21 07:38:59 +0300

    next module

--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   // tslint:disable-next-line: component-selector
   selector: 'events-app',
-  template: '<h1>Hello world</h1>',
+  template: `<h1>Hello world</h1>
+  <img src="/assets/images/basic-shield.png"/>`,
 })
 export class EventsAppComponent {
   title = 'ng-fundamental';
