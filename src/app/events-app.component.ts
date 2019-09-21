c3251947304a0428c7035adeb9642050862cba89
Simon Peter Ojok <simonojok19@gmail.com>
2019-09-21 08:13:38 +0300

    added componet

--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   // tslint:disable-next-line: component-selector
   selector: 'events-app',
-  template: `<h1>Hello world</h1>
-  <img src="/assets/images/basic-shield.png"/>`,
+  template: `<events-list></events-list>
+ `,
 })
 export class EventsAppComponent {
   title = 'ng-fundamental';
